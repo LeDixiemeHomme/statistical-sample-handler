baddfd43c770e6b0797cae8174fb4897893863c4
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-01-12 13:31:29 +0100

    spring boot structure

---
+++ src/test/java/fr/esgi/fyc/statisticalsamplehandler/StatisticalSampleHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.fyc.statisticalsamplehandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticalSampleHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
