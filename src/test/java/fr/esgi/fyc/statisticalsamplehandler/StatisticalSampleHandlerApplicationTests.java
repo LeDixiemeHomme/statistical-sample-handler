bb672c9deb0e8340838d1d5c44b547371212bb21
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-01-12 20:01:47 +0100

    fix mvn clean install

--- src/test/java/fr/esgi/fyc/statisticalsamplehandler/StatisticalSampleHandlerApplicationTests.java
+++ src/test/java/fr/esgi/fyc/statisticalsamplehandler/StatisticalSampleHandlerApplicationTests.java
@@ -1,9 +1,12 @@
 package fr.esgi.fyc.statisticalsamplehandler;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = StatisticalSampleHandlerApplicationTests.class)
 class StatisticalSampleHandlerApplicationTests {
 
     @Test
